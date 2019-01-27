edd5bd58e61456c4a3fa24596003d41eb94be9ca
meltz <soh@gmail.com>
2019-01-27 23:10:43 +0800

    JAn 27 update

---
+++ vue-cli/src/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+        <h1>Server status: {{ status }}</h1>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            status: 'Critial'
+        }
+    },
+    methods: {
+        changeStatus() {
+            this.status = 'Normal'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
